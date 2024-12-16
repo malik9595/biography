f30ef1e5d73cb5815993408212206fdbcb5f8b6a
Md Abdul Malik <mailerfakemail@gmail.com>
2024-12-16 08:58:35 +0530

    adding sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,10 @@
-next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://biography-gilt.vercel.app/', 
-    generateRobotsTxt: true, 
-    exclude: ['/path-to-exclude'],
-  };
-  
+  siteUrl: 'https://biography-gilt.vercel.app/', // Replace with your site's URL
+  generateRobotsTxt: true,           // Generates a robots.txt file
+  changefreq: 'daily',               // How frequently your pages are updated
+  priority: 0.7,                     // Priority of your pages (0.1 to 1.0)
+  sitemapSize: 5000,                 // Maximum number of URLs per sitemap file
+  exclude: ['/admin/*', '/secret'],  // Exclude specific pages or routes
+};
