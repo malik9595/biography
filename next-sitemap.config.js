e353a1d4e16da96377b02f3ad9a519e5612cc91a
Md Abdul Malik <mailerfakemail@gmail.com>
2024-12-16 09:59:39 +0530

    add and build

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://biography-gilt.vercel.app/', // Replace with your site's URL
+  siteUrl: 'https://biography-zp2o.vercel.app/', // Replace with your site's URL
   generateRobotsTxt: true,           // Generates a robots.txt file
   changefreq: 'daily',               // How frequently your pages are updated
   priority: 0.7,                     // Priority of your pages (0.1 to 1.0)
